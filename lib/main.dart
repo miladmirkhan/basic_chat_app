64c55dae2e6d1b9f6183eda7be1971af8133c256
milad <msherwani92@gmail.com>
2022-05-01 14:03:03 +0300

    handle the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
 import 'package:basic_chat_app/Screens/auth_screen.dart';
+import 'package:basic_chat_app/Screens/homeScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const meterial());
+  runApp( meterial());
 }
 
 class meterial extends StatelessWidget {
-  const meterial({ Key? key }) : super(key: key);
+ 
+Future<Widget> userSignedIn()async{//if user exist go to Home,not goto auth
+  User? user=FirebaseAuth.instance.currentUser;
+
+  if(user!=null){
+    return HomeScreen();
+  }else{
+    return AuthScreen();
+  }
+
+}
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AuthScreen(),
+      home: FutureBuilder(
+        future: userSignedIn(),
+        builder: (context,AsyncSnapshot<Widget> snapshot){
+          if(snapshot.hasData){
+            return snapshot.data!;
+          }
+          return Scaffold(
+            body:Center(child: CircularProgressIndicator())
+          );
+        },
+      ),
       
     );
   }
