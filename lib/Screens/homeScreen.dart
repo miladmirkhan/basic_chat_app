9a35077c73b4de57e769d46f2ee2b15f5e271ec0
milad <msherwani92@gmail.com>
2022-05-01 13:54:51 +0300

    done with homeScreen UI

---
+++ lib/Screens/homeScreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text("Home"),
+      centerTitle: true,
+      backgroundColor: Colors.teal,
+      actions: [
+        IconButton(
+          icon: Icon(Icons.logout),
+           onPressed: (){
+             
+           },)
+      ], 
+    ),
+      
+
+floatingActionButton: FloatingActionButton(
+  child: Icon(Icons.search),
+
+  onPressed: (){},
+
+    ));
+  }
+}
